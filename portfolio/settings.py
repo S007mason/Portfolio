9377d42c7599fab3f9d1e8983982a239257b4983
Mason <137528263+S007mason@users.noreply.github.com>
2024-11-21 17:36:35 +0100

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default="Mason")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mason-4cs3.onrender.com']
+
+
 
 
 RENDER_EXTERNAL_HOSTNAME=os.environ.get("RENDER_EXTERNAL_HOSTNAME")
