1982894f25f4c8b51b7395ecd4b1785982fa20ad
Mason <137528263+S007mason@users.noreply.github.com>
2025-02-02 15:08:42 +0100

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,33 +1,5 @@
-from django.core.paginator import Paginator
-from django.http import JsonResponse
+# myapp/views.py
 from django.shortcuts import render
-from .models import Proyectos
 
-
-
-from django.shortcuts import render
-from .models import Proyectos
-
-def lenguajes_view(request):
-    proyectos = Proyectos.objects.all()
-    offset = int(request.GET.get('offset', 0))  # Obtener el desplazamiento desde la URL, predeterminado a 0
-    limit = 3  # Número de proyectos a mostrar por página
-
-    proyectos_visible = proyectos[offset:offset + limit]  # Seleccionar el subconjunto de proyectos
-    total_proyectos = proyectos.count()  # Contar todos los proyectos
-
-    # Calcular si hay más elementos a la derecha o izquierda
-    hay_anterior = offset > 0
-    hay_siguiente = offset + limit < total_proyectos
-
-    # Calcular los nuevos offsets
-    offset_anterior = max(0, offset - limit)
-    offset_siguiente = offset + limit
-
-    return render(request, 'portfolio/index.html', {
-        'proyectos_visible': proyectos_visible,
-        'hay_anterior': hay_anterior,
-        'hay_siguiente': hay_siguiente,
-        'offset_anterior': offset_anterior,
-        'offset_siguiente': offset_siguiente,
-    })
+def index(request):
+    return render(request, 'index.html')
