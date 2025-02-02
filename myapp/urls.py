565fb8a203506471caaa11e7f73f68bc6459b8e1
Mason <137528263+S007mason@users.noreply.github.com>
2025-02-02 15:21:29 +0100

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,24 +1,7 @@
 from django.urls import path
-from . import views  # Esto importa las vistas desde el archivo views.py
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path
-from . import views
-
-from django.urls import path
-from . import views
-
-from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.lenguajes_view, name='index'),  # Página principal
+    # Cambiamos 'lenguajes_view' por 'index' (o por el nombre correcto de tu función)
+    path('', views.index, name='index'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
